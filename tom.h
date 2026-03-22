1b09e40e2257404237a1df851efc68d5e9896c4d
Lucas Arcoverde <lucasamelo@proton.me>
2026-03-22 10:34:26 -0300

    create some util functions.

--- tom.h
+++ tom.h
@@ -1,7 +1,12 @@
-#define MAX_TASKS       100
-#define MAX_TITLE_LEN   100
-#define MAX_DESC_LEN    100
-#define MAX_NAME_LEN    100
+#include <stdlib.h>
+
+#ifndef TOM_H
+#define TOM_H
+
+#define MAX_TASKS           100
+#define MAX_TITLE_LEN       100
+#define MAX_DESC_LEN        100
+#define MAX_NAME_LEN        100
 
 typedef struct {
     char title[MAX_TITLE_LEN];
@@ -13,3 +18,12 @@ typedef struct {
     char desc[MAX_DESC_LEN];
 } ToDoList;
 
+#define GET_ARRAY_LEN(x)    sizeof(x) / sizeof((x)[0])
+
+void clear_screen();
+void save_data();
+void load_data();
+void display_help_message();
+void display_program_version();
+
+#endif
