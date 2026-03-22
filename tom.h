c2912f08171edb375bc02da5f856b367212f26f3
Lucas Arcoverde <lucasamelo@proton.me>
2026-03-22 10:49:43 -0300

    create operation functions for manage task and todo lists.

--- tom.h
+++ tom.h
@@ -1,29 +1,47 @@
-#include <stdlib.h>
-
 #ifndef TOM_H
 #define TOM_H
 
-#define MAX_TASKS           100
-#define MAX_TITLE_LEN       100
-#define MAX_DESC_LEN        100
-#define MAX_NAME_LEN        100
+#include <stddef.h>
 
+#define MAX_TASKS      100
+#define MAX_TITLE_LEN  100
+#define MAX_DESC_LEN   100
+#define MAX_NAME_LEN   100
+
+// TASK STRUCT
 typedef struct {
     char title[MAX_TITLE_LEN];
-    char desc[MAX_DESC_LEN];
+    char desc[MAX_DESC_LEN];   // description
 } Task;
 
+// TODO LIST STRUCT
 typedef struct {
     char name[MAX_NAME_LEN];
-    char desc[MAX_DESC_LEN];
+    char desc[MAX_DESC_LEN];   // description
+    Task tasks[MAX_TASKS];
+    int task_count;
 } ToDoList;
 
-#define GET_ARRAY_LEN(x)    sizeof(x) / sizeof((x)[0])
+#define GET_ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
 
-void clear_screen();
-void save_data();
-void load_data();
-void display_help_message();
-void display_program_version();
+// UTILITY FUNCTIONS
+void clear_screen(void);
+void save_data(void);
+void load_data(void);
+void display_help_message(void);
+void display_program_version(void);
+
+// TODO LIST OPERATIONS
+void create_todo_list(const char *name, const char *desc);
+void view_todo_lists(void);
+void edit_todo_list(const char *name);
+void delete_todo_list(const char *name);
+
+// TASK OPERATIONS
+void add_task_to_list(const char *list_name, const char *title, const char *desc);
+void view_tasks(const char *list_name);
+void edit_task(const char *list_name, int task_index);
+void delete_task(const char *list_name, int task_index);
+void complete_task(const char *list_name, int task_index);
 
 #endif
